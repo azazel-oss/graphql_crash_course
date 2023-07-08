0ff91862485d996f0b64098869948c0e5ac39a65
Asad Mahmood <asadmhmd1234@gmail.com>
2023-07-08 21:26:13 +0530

    Add resolvers and type defs

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+
+// Database
+import db from "./_db.js";
 
 // Server setup
 
-const server = new ApolloServer({});
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.authors.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === game.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
